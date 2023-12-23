b32fddca02e2814c9a73121e9024fee68d6ce501
MohamedHamdi12 <129444345+MohamedHamdi12@users.noreply.github.com>
2023-12-23 22:13:41 +0100

    Ajout des tests sur le nouveau martin et base de cycles. Modification du retour des fonctions (on retourne le x optimal et son objectif desormais)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import solvepl
+import cycles
 
 PATH_TO_CPLEX = r'C:\Program Files\IBM\ILOG\CPLEX_Studio2211\cplex\bin\x64_win64\cplex.exe'
 
@@ -47,53 +48,66 @@ def draw_tree(nb_node, x):
     pos = nx.spring_layout(G)
     nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=700, node_color='skyblue', font_size=8,
             font_color='black', arrowsize=10)
+
     plt.show()
 
 def main():
     """
     Fonction principale exécutant les étapes principales du script.
     """
-    file_path = 'instances/Spd_Inst_Rid_Final2/Spd_RF2_20_27_219.txt'  # Remplacez par le chemin de votre fichier
+    file_path = 'instances/Spd_Inst_Rid_Final2/Spd_RF2_20_27_235.txt'  # Remplacez par le chemin de votre fichier
     graph = read_graph_from_file(file_path)
     directed_g = graph.to_directed()
 
-    print("Liste des arêtes du graphe :", graph.edges())
-    print("Liste des arêtes du graphe :", directed_g.edges())
+    #print("Liste des arêtes du graphe :", graph.edges())
+    #print("Liste des arêtes du graphe :", directed_g.edges())
 
     nx.draw(graph, with_labels=True, font_weight='bold')
     plt.show()
 
-    #nx.draw(directed_g, with_labels=True, font_weight='bold')
-    #plt.show()
+    nx.draw(directed_g, with_labels=True, font_weight='bold')
+    plt.show()
 
     time_limit = 60
 
-    x, y = solvepl.pl_flot(directed_g, time_limit, PATH_TO_CPLEX)
-    draw_tree(graph.number_of_nodes(), x)
+    x, z = solvepl.pl_flot(directed_g, time_limit, PATH_TO_CPLEX)
+    print('flot1: ',z)
+    #draw_tree(graph.number_of_nodes(), x)
     # Afficher les valeurs de y
-    for i, var in y.items():
-        print(f"{var.name} = {var.value()}")
+    #for i, var in y.items():
+     #   print(f"{var.name} = {var.value()}")
 
-    for i, var in x.items():
-        print(f"{var.name} = {var.value()}")
+    #for i, var in x.items():
+    #    print(f"{var.name} = {var.value()}")
 
-    x, y = solvepl.pl_flot_multi(directed_g, time_limit, PATH_TO_CPLEX)
-    draw_tree(graph.number_of_nodes(), x)
+    x, z = solvepl.pl_flot_multi(directed_g, time_limit, PATH_TO_CPLEX)
+    print('flot2: ',z)
+    #draw_tree(graph.number_of_nodes(), x)
     # Afficher les valeurs de y
-    for i, var in y.items():
-        print(f"{var.name} = {var.value()}")
+    #for i, var in y.items():
+    #    print(f"{var.name} = {var.value()}")
 
-    for i, var in x.items():
-        print(f"{var.name} = {var.value()}")
+    #for i, var in x.items():
+    #    print(f"{var.name} = {var.value()}")
 
     x, z = solvepl.pl_martin(graph, time_limit, PATH_TO_CPLEX)
+    print('martin1: ',z)
+    #draw_tree(graph.number_of_nodes(), x)
+
+    #for i, var in z.items():
+     #   print(f"{var.name} = {var.value()}")
+
+    #for i, var in x.items():
+    #   print(f"{var.name} = {var.value()}")
+
+    x, z = solvepl.pl_martin2(graph, time_limit, PATH_TO_CPLEX)
+    print('martin2: ', z)
     draw_tree(graph.number_of_nodes(), x)
 
-    for i, var in z.items():
-        print(f"{var.name} = {var.value()}")
 
-    for i, var in x.items():
-        print(f"{var.name} = {var.value()}")
+    x, z, graph = cycles.solve_by_cycles(graph, time_limit, PATH_TO_CPLEX)
+    print('cycles: ',z)
+    draw_tree(graph.number_of_nodes(), x)
 
     return 0
 
