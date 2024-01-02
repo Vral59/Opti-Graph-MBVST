e36dc413a27126220e6c46d4590ed0700647ceb9
MohamedHamdi12 <129444345+MohamedHamdi12@users.noreply.github.com>
2024-01-02 13:41:40 +0100

    nouvelle correction pour la convergence

--- cycles.py
+++ cycles.py
@@ -16,14 +16,15 @@ def destruct_cycles(graph, time_limit, path_to_cplex):
        @return: La variables de décision obtenue (x), l'objectif obtenue et le graphe obtenue.
        """
 
+    res_graph = copy.deepcopy(graph)
     
     solver = pl.CPLEX_CMD(path=path_to_cplex, timeLimit=time_limit, logPath="info.log", msg=False)
     model = pl.LpProblem("main_problem", pl.LpMinimize)
 
     # Nombre de sommet
-    nb_nodes = graph.number_of_nodes()
-    cycles = nx.cycle_basis(graph)
-    edges = graph.edges
+    nb_nodes = res_graph.number_of_nodes()
+    cycles = nx.cycle_basis(res_graph)
+    edges = res_graph.edges
 
     # Création des variables
     x = {e: pl.LpVariable(cat=pl.LpBinary, name="x_{0}".format(e)) for e in edges}
@@ -47,7 +48,7 @@ def destruct_cycles(graph, time_limit, path_to_cplex):
 
     # Contrainte (5)
     for v in range(1, nb_nodes + 1):
-        model += pl.lpSum(x[e] for e in edges_containing_node(graph, v)) - 2 <= graph.degree[v] * y[v]
+        model += pl.lpSum(x[e] for e in edges_containing_node(res_graph, v)) - 2 <= res_graph.degree[v] * y[v]
         model.solve(solver)
 
     model.solve(solver)
@@ -56,14 +57,13 @@ def destruct_cycles(graph, time_limit, path_to_cplex):
 
     for edge in edges:
         if not x[edge].value():
-            graph.remove_edge(edge[0], edge[1])
+            res_graph.remove_edge(edge[0], edge[1])
+
+    return x, pl.value(model.objective), res_graph
 
 
-    return x, pl.value(model.objective), graph
 
-
-
-def link_components(original_graph, graph):
+def link_components(original_graph, pl_graph):
     """
        Rélie les composantes connexes du graphe
 
@@ -71,17 +71,18 @@ def link_components(original_graph, graph):
        @return: Le graphe obtenue après ajout des arêtes entre les paires de noeuds qui sont dans des composantes différentes
     """
     original_edges = original_graph.edges
-    connected_components = list(nx.connected_components(graph))
+    res_graph = copy.deepcopy(pl_graph)
+    connected_components = list(nx.connected_components(res_graph))
     components_number = len(connected_components)
 
     for n1 in range(components_number - 1):
         comp1 = connected_components[n1]
         for n2 in range(n1 + 1, components_number):
             comp2 = connected_components[n2]
-            graph.add_edges_from([(node1, node2) for node1 in comp1 for node2 in comp2 
+            res_graph.add_edges_from([(node1, node2) for node1 in comp1 for node2 in comp2 
                         if (node1,node2) in original_edges or (node2,node1) in original_edges])
 
-    return graph
+    return res_graph
 
 
 
@@ -95,17 +96,19 @@ def solve_by_cycles(graph, time_limit, path_to_cplex):
        @param path_to_cplex: Chemin vers CPLEX.
        @return: La variables de décision (x), l'objectif et le graphe obtenues.
        """
-    graph_res = copy.deepcopy(graph)
-    original_graph = copy.deepcopy(graph_res)
+       
     start_time = time.time()
-    x, z, graph_res = destruct_cycles(graph_res, time_limit, path_to_cplex)
-    connected = nx.is_connected(graph_res)
+    
+    connex_graph = copy.deepcopy(graph)
+    x, z, pl_graph = destruct_cycles(connex_graph, time_limit, path_to_cplex)
+    connected = nx.is_connected(pl_graph)
+    if not connected:
+        connex_graph = link_components(connex_graph, pl_graph)
     
     while not connected and (time.time()-start_time < time_limit):
-        x, z, graph_res = destruct_cycles(graph_res, time_limit, path_to_cplex)
-        connected = nx.is_connected(graph_res)
+        x, z, pl_graph = destruct_cycles(connex_graph, time_limit, path_to_cplex)
+        connected = nx.is_connected(pl_graph)
         if not connected:
-            graph_res = link_components(original_graph, graph_res)
-            original_graph = copy.deepcopy(graph_res)
-
-    return x, z, graph_res
+            connex_graph = link_components(connex_graph, pl_graph)
+    
+    return x, z, connex_graph
